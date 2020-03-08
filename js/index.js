ab1c7213ec4757ce49d0826c02bab7815fa209c1
Corey Knoettgen <cknoettg@yahoo.com>
2020-03-08 18:19:51 -0400

    Set up initial project and added two events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+//Sample:
+// let sampleButton = document.querySelector('.sampleButton');
+// sampleButton.addEventListener('click', (event) => {
+//   alert('You Clicked the Sample Button!')
+// })
+
+let mouseTitle = document.querySelector('.logo-heading');
+mouseTitle.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'red';
+});
+
+let keyImg = document.querySelector('.intro img');
+keyImg.addEventListener('keydown', (event) => {
+    if (event.code == "KeyA") {
+        event.target.style.border = '2px solid blue';
+    }
+});
+
